f7203d78ead619fdca37b2b3757af740dd4d5007
Magnus Herweyer <magnus.herweyer@gmail.com>
2022-08-15 20:47:41 -0500

    Finishes bubblesort

--- bubblesort.py
+++ bubblesort.py
@@ -12,10 +12,34 @@ def BubbleSort(unsorted_array):
             if unsorted_array[j] > unsorted_array[j+1]:
                 temp = unsorted_array[j]
                 unsorted_array[j] = unsorted_array[j+1]
-                unsorted_array[j+1] = temp 
+                unsorted_array[j+1] = temp
+
+
+
+
+
+
+
+
+
+
+def BubbleSortForMorePractice(ArrayToSort):
+    ArrayLength = len(ArrayToSort)
+
+    for i in range(ArrayLength):
+        for j in range(0, ArrayLength - i - 1):
+            if(ArrayToSort[j] > ArrayToSort[j+1]):
+                temp = ArrayToSort[i]
+                ArrayToSort[j] = ArrayToSort[j+1]
+                ArrayToSort[j+1] = temp
+
+
+def BubbleSortThirdPractice(ArrayToCall):
+    pass
+
 
 if __name__ == '__main__':
     
-    array = [ 1, 0, 3, 2 ,5, 4] 
-    BubbleSort(array)
+    array = [ 1, 0, 3, 4, 4, 4, 2 ,5, 4, -1, -3, 9, 11]
+    BubbleSortForMorePractice(array)
     print(array)
